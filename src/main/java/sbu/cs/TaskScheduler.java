3af6af8d4ed70bec24ca7fab93a140fa47039c1c
unknown <peymanick@gmail.com>
2024-05-12 19:55:22 +0330

    completedOnMain

--- src/main/java/sbu/cs/TaskScheduler.java
+++ src/main/java/sbu/cs/TaskScheduler.java
@@ -1,7 +1,7 @@
 package sbu.cs;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.desktop.ScreenSleepEvent;
+import java.util.*;
 
 public class TaskScheduler
 {
@@ -27,13 +27,14 @@ public class TaskScheduler
             TODO
                 Simulate utilizing CPU by sleeping the thread for the specified processingTime
              */
+            try{ Thread.sleep(processingTime); }
+            catch(InterruptedException e) { System.out.println("Thread intrrupted"); }
         }
     }
 
     public static ArrayList<String> doTasks(ArrayList<Task> tasks)
     {
         ArrayList<String> finishedTasks = new ArrayList<>();
-
         /*
         TODO
             Create a thread for each given task, And then start them based on which task has the highest priority
@@ -41,7 +42,24 @@ public class TaskScheduler
             You have to wait for each task to get done and then start the next task.
             Don't forget to add each task's name to the finishedTasks after it's completely finished.
          */
+        ArrayList<Task> tasksCopy = new ArrayList<>();
+        for (int i=0 ; i< tasks.size() ; i++)
+            tasksCopy.add(tasks.get(i));
 
+        for (int i=0 ; i < tasks.size() ; i++){
+            int maxIndex = 0;
+            for (int j=0 ; j<tasksCopy.size() ; j++)
+                if (tasksCopy.get(j).processingTime > tasksCopy.get(maxIndex).processingTime)
+                    maxIndex = j;
+
+            Thread thread = new Thread(tasksCopy.get(maxIndex));
+            thread.start();
+            try { thread.join(); }
+            catch (Exception e){ System.out.println("Error"); }
+
+            finishedTasks.add(tasksCopy.get(maxIndex).taskName);
+            tasksCopy.remove(maxIndex);
+        }
         return finishedTasks;
     }
 
